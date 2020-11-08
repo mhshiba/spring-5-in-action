fa8cd6a06e3259f1e86fe4c6e29a87bbb3f6c7a8
Marcelo Hiroshi Shiba <marcelo.shiba@gmail.com>
2020-11-08 12:35:38 -0300

    Implementa com JDBC Template

---
+++ src/test/java/com/example/demo/HomeControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import tacos.TacoCloudApplication;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = TacoCloudApplication.class)
+//@SpringBootTest(classes=TacoCloudApplication.class)
+@WebMvcTest
+public class HomeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testHomePage() throws Exception {
+		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("home"))
+				.andExpect(content().string(containsString("Welcome to...")));
+	}
+}
---
+++ src/test/java/com/example/demo/TacoCloudApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tacos.TacoCloudApplication;
+
+@SpringBootTest(classes = TacoCloudApplication.class)
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
